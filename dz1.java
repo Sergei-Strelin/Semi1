e45b4fcc0124267864376db577b64ca913402931
Sergei-Strelin <112539172+Sergei-Strelin@users.noreply.github.com>
2023-01-27 15:27:17 +0300

    Update dz1.java

--- dz1.java
+++ dz1.java
@@ -4,34 +4,34 @@ import javax.print.event.PrintEvent;
 import java.util.Scanner;
 
 public class dz1 {
-    private    = new Scanner (System.in ); 
+    private static Scanner input = new Scanner(System.in); 
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int number = getUser("Введите номер треугольного числа: ");
         int numberTriangle = triangle(number);
-        printTrangle(numberTriangle, "Треугольное число под № " + number + " = "); 
+        printTrangle(numberTriangle, "Треугольное число под № " + number + " = ");        
     }
 
     
-    static int getUser(String ){
+    static int getUser(String text){
         System.out.print(text);
         return input.nextInt();
     }
 
 
     
-    static int triangle(int ) {
+    static int triangle(int number) {
         if (number == 1){
-            1 1;
+            return 1;
         }
         else{
-            return (number + triangle (number-1));
+            return (number + triangle(number-1));
         }
     }
 
 
     
-    static String printTrangle(int number, String ){
+    static String printTrangle(int number, String text){
         System.out.println(text + number);
         return null;
     }
